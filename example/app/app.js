c14372365cf99bbd4d801a108167bd587c5c7d79
Martin Pfund <m.pfund@mid.de>
2022-07-12 14:13:07 +0200

    chore(lint): Fixed lint errors

--- example/app/app.js
+++ example/app/app.js
@@ -52,14 +52,14 @@ document.getElementById('js-toggle-fullscreen').addEventListener('click', functi
   }
 });
 
-/*document.getElementById('js-toggle-keyboard-help').addEventListener('click', function() {
+/* document.getElementById('js-toggle-keyboard-help').addEventListener('click', function() {
   state.keyboardHelp = !state.keyboardHelp;
   let displayProp = 'none';
   if (state.keyboardHelp) {
     displayProp = 'block';
   }
   document.getElementById('io-dialog-main').style.display = displayProp;
-}); 
+});
 */
 
 document.getElementById('io-dialog-main').addEventListener('click', function() {
